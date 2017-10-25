7661f94148ca3f4eb919f88b54151bb559a4bd40
penglei <pengleihfut@gmail.com>
2017-10-25 20:11:00 +0800

    up

--- qcloud/main.js
+++ qcloud/main.js
@@ -1,15 +1,9 @@
 const http = require('http');
 
-
-const hostname = '127.0.0.1';
-const port = 3000;
+xxxxxxxxxxxxxxxxxxxxxo
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('1111111111111111111');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  res.end('222222222222222222222');
 });
